7d632b40b73a894036931664a7a1e2ce7b120ff3
adityajoe <102590926+adityajoe@users.noreply.github.com>
2026-06-21 14:18:12 +0530

    Update query_sum.py

--- Arrays/query_sum.py
+++ Arrays/query_sum.py
@@ -1,4 +1,8 @@
-#User function Template for python3
+# Given a set of activities, each with a start time and a finish time, represented by the arrays start[] and finish[], respectively. 
+# A single person can perform only one activity at a time, meaning no two activities can overlap. 
+# Your task is to determine the maximum number of activities that a person can complete in a day.
+# Note: Start time and finish time cannot overlap, i.e., if a person finishes an activity at time x, then they cannot start another activity at time x.
+
 
 class Solution:
     def querySum(self, n, arr, q, queries):
